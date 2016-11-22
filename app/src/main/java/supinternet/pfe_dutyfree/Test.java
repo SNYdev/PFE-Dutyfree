68bb6404e88310f524102734551167e92e8b8fe2
SNYdev <yanis.sahnoune@supinternet.fr>
2016-11-22 18:44:04 +0100

    add test activity

---
+++ app/src/main/java/supinternet/pfe_dutyfree/Test.java
@@ -0,0 +1,17 @@
+package supinternet.pfe_dutyfree;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        TextView testLanguage = (TextView) findViewById(R.id.testLanguage);
+        testLanguage.setText(R.string.hello_world);
+    }
+}
